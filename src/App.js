72e569ff2252a61f87489b00ff80b51300920855
FuTkY <thedanciman@gmail.com>
2023-01-14 16:06:42 +0100

    ponovno

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from "react";
-
+import logo from './logo.svg';
 import './App.css';
-
-import Helper from "./Helper.js";
-import Header from "./Header.js";
-import Mapa from "./Mapa";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 function App() {
   return (
-    <div className="App">
-      {/*DIV*/}
-
-        <Header/>
-        <Mapa/>
-        <Helper/>
-      {/*DIV*/}
-
-
-
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
